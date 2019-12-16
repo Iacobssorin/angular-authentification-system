7d33aabe2e053b56a5b1cd1043451f3b95278ba0
iacobssorin <iacobs205@gmail.com>
2019-12-16 21:09:55 +0100

    first hour

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Hello {{title}}</h1><messages></messages>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my ';
+}
---
+++ frontend/src/app/messages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+
+@Component({
+    template: 'this is the messages component'
+
+})
+
+export class MessagesComponent {
+    
+}
